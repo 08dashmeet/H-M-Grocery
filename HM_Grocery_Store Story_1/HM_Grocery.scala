6fe04958caf8ecbd2e5f9b588ac7508cc5c6791e
08dashmeet <35292258+08dashmeet@users.noreply.github.com>
2018-03-05 11:59:04 +0530

    Story 3 & 4
    3. As a guest User i should be able to add/update/remove items to/from Personal Cart
    4. As a guest User i should be able to checkout Cart items and that should be reflected in catalogue as well

--- HM_Grocery_Store Story_1/HM_Grocery.scala
+++ HM_Grocery_Store Story_1/HM_Grocery.scala
@@ -2,12 +2,14 @@ package HM_Grocery_Store
 
 import scala.xml._
 
+
+
 object HM_Grocery extends App{
   case class Items(id : Int,name : String,uom :String,unitsize:Int,unitprice:String,quantity:String)
 
   case class unitprice(amount : Double,currency:String)
   case class Quantity(stock :Int,measure:String)
-
+  var total_bill=0.0
   def show_unitprice(n:Node) :unitprice = {
     var amount = (n\\"amount").text.toDouble
      var currency = (n \\ "currency").text
@@ -39,7 +41,62 @@ object HM_Grocery extends App{
 
   }
 
+                         // Story 3
+
+
+  case class Item_toCart(product:Array[String],Q :Array[Int])
+
+   def Add_Cart(): Unit = {
+
+
+     println("How many items you wanna buy-: ")
+     val n = readInt()
+     val itemname = new Array[String](n)
+     val item_quan = new Array[Int](n)
+     var sum= new Array[Double](n)
+
+     for (i <- 0 until n) {
+       println("Choose any Item from Catalogue -: ")
+       itemname(i) = readLine()
+
+       println("Enter the Quantity of Item -: ")
+       item_quan(i) = readInt()
+
+
+       for(j <- 0 until 4){
+        if(itemname(i) == item(j).name){
+          itemname(i)=("Item Name-: " + item(j).name + ", " + "Price-: " + item(j).unitprice + " ,Quantity -: ")
+          var total = (item(j).unitprice).split(" ")
+          sum(i)= (total(0).toDouble) * item_quan(i)
+          total_bill += sum(i)
+        }
+      }
+
+       }
+
+       val li = (itemname, item_quan,sum).zipped            // print items in cart
+       println("Products added in Cart are -: ")
+         li.toList.foreach(println)
+
+         println("Do you want to Checkout y/n")          // Checkout Cart
+          val ans = readLine()
+         if(ans=="y"){
+          println("Your Cart Items Are -:")
+           li.toList.foreach(println)
+          println("The total Amount to be paid is-:"+total_bill)
+         }
+
+ }
+
+
   val xml_file = XML.loadFile("/home/dashmeet/IdeaProjects/scala practice/src/HM_Grocery_Store/HM_Data.xml")
   var item =  (xml_file \\ "item").map(showItems)
+  println("                        Welcome to H&M Grocery Store                      ")
+
+  println("H&M's Catalogue -: ")
   item.foreach(println)
+ 
+   Add_Cart
+
+
 }
