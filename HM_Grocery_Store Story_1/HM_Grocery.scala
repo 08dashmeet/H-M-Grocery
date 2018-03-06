cddd8a3256c1d9956479ca0f222a7eda4f9f8b4d
08dashmeet <35292258+08dashmeet@users.noreply.github.com>
2018-03-06 12:15:46 +0530

    Story 3 & 4 with update functionality

--- HM_Grocery_Store Story_1/HM_Grocery.scala
+++ HM_Grocery_Store Story_1/HM_Grocery.scala
@@ -1,5 +1,6 @@
 package HM_Grocery_Store
 
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.xml._
 
 
@@ -48,44 +49,94 @@ object HM_Grocery extends App{
 
    def Add_Cart(): Unit = {
 
-
      println("How many items you wanna buy-: ")
      val n = readInt()
-     val itemname = new Array[String](n)
+     var itemname = ListBuffer[String]()
+     var iname = new Array[String](n)
      val item_quan = new Array[Int](n)
-     var sum= new Array[Double](n)
+     var sum = new Array[Double](n)
 
      for (i <- 0 until n) {
        println("Choose any Item from Catalogue -: ")
-       itemname(i) = readLine()
+       // itemname(i) = readLine()
+       iname(i) = readLine()
 
        println("Enter the Quantity of Item -: ")
        item_quan(i) = readInt()
 
 
-       for(j <- 0 until 4){
-        if(itemname(i) == item(j).name){
-          itemname(i)=("Item Name-: " + item(j).name + ", " + "Price-: " + item(j).unitprice + " ,Quantity -: ")
-          var total = (item(j).unitprice).split(" ")
-          sum(i)= (total(0).toDouble) * item_quan(i)
-          total_bill += sum(i)
-        }
-      }
-
+       for (j <- 0 until 4) {
+         if (iname(i) == item(j).name) {
+           itemname += ("Item Name-: " + item(j).name + ", " + "Price-: " + item(j).unitprice + " ,Quantity -: ").toString()
+           var total = (item(j).unitprice).split(" ")
+           sum(i) = (total(0).toDouble) * item_quan(i)
+           total_bill += sum(i)
+         }
        }
+       //             println("Do you wanna Delete this item y/n")
+       //              var userans = readLine()
+       //              if(userans == "y"){
+       ////                println("Enter the name of item to be deleted")
+       ////                var del = readLine()
+       ////                if(del== iname(i)){
+       //                  itemname -= itemname(i)
+       //
+       //              }
 
-       val li = (itemname, item_quan,sum).zipped            // print items in cart
-       println("Products added in Cart are -: ")
-         li.toList.foreach(println)
 
-         println("Do you want to Checkout y/n")          // Checkout Cart
+     }
+
+     val li = (itemname, item_quan, sum).zipped.toList             // print items in cart
+     println("Products added in Cart are -: ")
+     li.foreach(println)
+
+      for(i <- 0 until n) {
+
+        println("Do you wanna update your Cart y/n") // Update the Cart
+        val usr = readLine()
+        if (usr == "y") {
+          println("Enter the product name to be Updated from Cart")
+          val update_name = readLine()
+          for (i <- 0 until n) {
+            if (update_name == iname(i)) {
+
+              println("Enter the Quantity")
+              val new_item_quan = readInt()
+              item_quan(i) = new_item_quan
+            }
+
+            for (j <- 0 until 4) {
+              if (iname(i) == item(j).name) {
+                itemname += ("Item Name-: " + item(j).name + ", " + "Price-: " + item(j).unitprice + " ,Quantity -: ").toString()
+                var total = (item(j).unitprice).split(" ")
+                sum(i) = (total(0).toDouble) * item_quan(i)
+                total_bill += sum(i)
+              }
+            }
+
+
+          }
+
+          println("Updated Cart is -:")
+          val li = (itemname, item_quan, sum).zipped
+          li.toList.foreach(println)
+
+        }
+
+
+      }
+         println("Do you want to Checkout y/n")                 // Checkout Cart
           val ans = readLine()
          if(ans=="y"){
-          println("Your Cart Items Are -:")
-           li.toList.foreach(println)
+          println("Your Items in Cart Are -:")
+           li.foreach(println)
           println("The total Amount to be paid is-:"+total_bill)
          }
 
+       println("Your Order Summery is ")                               // Order Summary
+        println("Items Purchased are -")
+          li.foreach(println)
+       println("The total Amount paid is-:"+ total_bill)
  }
 
 
@@ -95,8 +146,6 @@ object HM_Grocery extends App{
 
   println("H&M's Catalogue -: ")
   item.foreach(println)
- 
+
    Add_Cart
-
-
 }
