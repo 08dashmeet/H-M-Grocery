33cd912bd9e17ff3ab26aa4b79dd423bd89c12c7
08dskaler <35292258+08dashmeet@users.noreply.github.com>
2018-03-07 19:30:30 +0530

    Add files via upload
    Story 6 & 7

--- HM_Grocery_Store Story_1/HM_Grocery.scala
+++ HM_Grocery_Store Story_1/HM_Grocery.scala
@@ -1,29 +1,31 @@
 package HM_Grocery_Store
+import java.util
+
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
 import scala.util.control.Breaks._
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.xml._
 
-
-
-object HM_Grocery extends App{
+class HM_Grocery  {
   case class Items(id : Int,name : String,uom :String,unitsize:Int,unitprice:String,quantity:String)
 
   case class unitprice(amount : Double,currency:String)
   case class Quantity(stock :Int,measure:String)
 
-  def show_unitprice(n:Node) :unitprice = {
+   def show_unitprice(n:Node) :unitprice = {
     var amount = (n\\"amount").text.toDouble
      var currency = (n \\ "currency").text
     unitprice(amount,currency)
   }
 
-  def show_quantity(n:Node) :Quantity = {
+   def show_quantity(n:Node) :Quantity = {
     var stock = (n\\"stock").text.toInt
     var measure = (n \\ "measure").text
     Quantity(stock,measure)
   }
 
-  def showItems(n:Node):Items={
+   def showItems(n:Node):Items={
 
     var id = (n \\ "id").text.toInt
     var name = (n \\ "name").text
@@ -51,6 +53,8 @@ object HM_Grocery extends App{
   var li = List[(String,Int)]()
   var n =0
   var  total_bill = 0.0
+  var discount=0.0
+  var ans=0.0
 
 
    def Add_Cart(): Unit = {
@@ -79,15 +83,43 @@ object HM_Grocery extends App{
      li.foreach(println)
  }
 
-  def Check_Cart: Unit ={
+
+  def Cart_discount: Unit ={
+                                                                        // Cart_discount function
+      var l= li.length
+      for(i <- 0 until l) {
+        for(j <- 0 until 4) {
+           if (iname(i)==(item(j).name)) {
+              var total = (item(j).unitprice).split(" ")
+
+             if (HM_Grocery.no == HM_Grocery.hm.get(iname(i)).get) {
+                   println(HM_Grocery.no + HM_Grocery.hm.get(iname(i)).get )
+                HM_Grocery.no match {
+                  case 1 => discount = (total(0).toDouble) * item_quan(i) * 0.60
+                  case 2 => discount = (total(0).toDouble) * item_quan(i) * 0.55
+                  case _ => discount = 0.0
+                }
+
+              }
+             
+             
+             
+
+           }
+        }
+    }
+
+  }
+
+  def Check_Cart: Unit ={                          //original
   println("Do you want to Checkout y/n")                 // Checkout_Cart function
   val ans = readLine()
   if(ans=="y"){
     println("Your Items in Cart Are -:")
     li.foreach(println)
-   var l= li.length
-    for(i <- 0 until l){
+     var l= li.length
 
+    for(i <- 0 until l){
       for (j <- 0 until 4) {
         if (iname(i)==(item(j).name)) {
           var total = (item(j).unitprice).split(" ")
@@ -96,7 +128,8 @@ object HM_Grocery extends App{
         }
       }
     }
-    println("The total Amount to be paid is-: "+ total_bill)
+
+    println("The total Amount to be paid is-: "+(total_bill-discount))
   }
 }
 
@@ -105,13 +138,15 @@ object HM_Grocery extends App{
     println("Your Order Summery is ")
     println("Items Purchased are -")
     li.foreach(println)
-    println("The total Amount to be paid is-: "+ total_bill)
+
+    println("The total Amount to be paid is-: "+ (total_bill-discount))
 
   }
 
 
-  def update_toCart: Unit = {                         // Update_Cart function
-    breakable {
+  def update_toCart: Unit = {                                 // Update_Cart function
+
+    breakable  {
       for (i <- 0 until n) {
         println("Do you wanna update your Cart Delete/Update Quantity/No")
         val usr = readLine()
@@ -141,15 +176,22 @@ object HM_Grocery extends App{
         }
 
         else {
-          break
-        }
 
+          break
+
+        }
+      }
       }
       println("Updated Cart is -:")
       li = (itemname, item_quan).zipped.toList
       li.foreach(println)
     }
-  }
+
+
+               // Story 6&7
+
+
+
   val xml_file = XML.loadFile("/home/dashmeet/IdeaProjects/scala practice/src/HM_Grocery_Store/HM_Data.xml")
   var item =  (xml_file \\ "item").map(showItems)
   println("                        Welcome to H&M Grocery Store                      ")
@@ -157,8 +199,57 @@ object HM_Grocery extends App{
   println("H&M's Catalogue -: ")
   item.foreach(println)
 
-   Add_Cart
-  update_toCart
-  Check_Cart
-  Order_Summary
+}
+
+
+object HM_Grocery extends App{
+
+       //var obj =  new HM_Grocery
+       var no = 0
+     var hm=Map[String,Int]()
+     val password = "1234"
+      println("Login As a Admin Or User")
+      var a = readLine()
+      if(a=="Admin") {
+        println("Enter the Password")
+        var p = readLine()
+        if (p == password) {
+          println("Welcome to Admin part")
+          //  obj.Add_Cart()
+          //  obj.update_toCart
+          println("Discounts on various Items -:")
+          println("1-: Get 60% discount in Lux Soap")
+          println("2-: Get 55% discount in Adidas Deodarant")
+          //        println("Choose the Discount or print 0")
+          //         no = readInt()
+          //        hm = Map("LUX Bath Soap"-> 1,  "Adidas Deodorant"->2,"Himalayan Neem FaceWash"->3,"Vasiline Body Lotion"->4)
+          //          obj.Cart_discount
+          //          obj.Check_Cart
+          //          obj.Order_Summary
+              }
+
+
+          else
+          {
+            println("Wrong Password")
+          }
+
+        }
+        else if (a == "User") {
+          var obj = new HM_Grocery
+          obj.Add_Cart()
+          obj.update_toCart
+          println("Discounts on various Items -:")
+          println("1-: Get 60% discount in Lux Soap")
+          println("2-: Get 55% discount in Adidas Deodarant")
+          println("Choose the Discount or print 0")
+          no = readInt()
+          hm = Map("LUX Bath Soap" -> 1, "Adidas Deodorant" -> 2, "Himalayan Neem FaceWash" -> 3, "Vasiline Body Lotion" -> 4)
+          obj.Cart_discount
+          obj.Check_Cart
+          obj.Order_Summary
+        }
+
+
+
 }
